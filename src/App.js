2346ff067fc9b01f9bee117c086ecafb7eff3e29
pgtechworkshop <you@example.com>
2021-05-08 23:16:02 +0530

    Home page with router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Route , Switch } from "react-router-dom";
+
+import Home from "./Home";
 
 function App() {
-return <div className="app">React App</div>
+return <div className="app">
+  <Router>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router> 
+  </div>
 }
 
 export default App;
