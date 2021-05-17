f415bb9bfb86f4ec8b95955746a18c34af8436f1
pgtechworkshop <you@example.com>
2021-05-18 01:56:01 +0530

    Added checkoutProduct

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,4 @@
+import { CallToActionRounded } from "@material-ui/icons";
 import { act } from "react-dom/test-utils";
 
 export const initialState = {
@@ -17,6 +18,36 @@ const reducer = (state,action) => {
             ...state,
             basket: [...state.basket, action.item],
         };
+    case 'EMPTY_BASKET':
+        return {
+            ...state,
+            basket: []
+        }
+    case "REMOVE_FROM_BASKET":
+        const index = state.basket.findIndex(
+            (basketItem) => basketItem.id === action.id
+        );
+        let newBasket = [...state.basket];
+
+        if(index >=0 ) {
+            newBasket.splice(index,1);
+        }
+        else
+        {
+            console.warn(
+                'cant remove product (id: ${action.id} as its not in basket!'
+            )
+        }
+        return {
+            ...state,
+            basket: newBasket
+        }
+
+    case 'SET_USER':
+        return {
+            ...state,
+            user: action.user
+        }
         default:
             return state;
     }
